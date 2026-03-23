a00ff67f628a08b540b637ddb566afe4f137824b
Chandru38 <s.2004chandru@gmail.com>
2026-03-23 21:58:18 +0530

    Update navbar1.jsx

--- src/components/navbar1.jsx
+++ src/components/navbar1.jsx
@@ -40,7 +40,7 @@ const Navbar = ({ theme, setTheme }) => {
         {/* 🔹 Desktop Collapse Button */}
         <div className="hidden sm:flex justify-between p-4">
             <button
-                onClick={() => setCollapsed(collapsed)}
+                onClick={() => setCollapsed(!collapsed)}
                 className="p-2 rounded hover:bg-surface-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-layout-sidebar-reverse" viewBox="0 0 16 16">
                     <path d="M16 3a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-5-1v12H2a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1zm1 0h2a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-2z"/>
